715c3976aa68e1f3761ab42ee02e07c45788564d
Desiree Lerma <desi.lerma96@yahoo.com>
2020-11-03 00:12:48 -0600

    commit tree aa4e925e2d56dfd5869828d91a39a9963fe673ae

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,12 @@
 # your code goes here
+class Person
+    def initialize(name)
+        @name = name
+    end
+
+    def person_name(name)
+        @name_hash[people] = {}
+        @name_hash.push (:name)
+    end
+
+end
